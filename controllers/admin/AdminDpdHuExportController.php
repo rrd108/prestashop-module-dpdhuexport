723ff62c6f95595092da53dd789e1d76e0f797ef
rrd <rrd@webmania.cc>
2017-03-09 10:44:15 +0100

    indentation changes

--- controllers/admin/AdminDpdHuExportController.php
+++ controllers/admin/AdminDpdHuExportController.php
@@ -41,7 +41,7 @@ class AdminDpdHuExportController extends ModuleAdminController
         parent::__construct();
     }
     
-    public function init()
+	public function init()
     {
         $this->number_from = Tools::getValue('number_from') ? Tools::getValue('number_from') : false;
         $this->number_to = Tools::getValue('number_to') ? Tools::getValue('number_to') : false;
@@ -92,16 +92,16 @@ class AdminDpdHuExportController extends ModuleAdminController
 				$invoices = $this->getByNumberInterval($this->number_from, $this->number_to);
                 if (count($invoices)) {
                     //generate XML
-                    $this->generateCSV($invoices);
-                }
+					$this->generateCSV($invoices);
+				}
                 else {
                     $this->errors[] = $this->l('No invoice has been found for this period.');
                 }
-            }
-        }
-        else
+			}
+		}
+		else
         {
-            parent::postProcess();          
+			parent::postProcess();          
         }
     }
     
@@ -136,39 +136,39 @@ class AdminDpdHuExportController extends ModuleAdminController
     private function generateCSV($collection)
     {
         // clean buffer
-        if (ob_get_level() && ob_get_length() > 0)
-            ob_clean();
-        $this->getList($this->context->language->id);
-        if (!count($this->_list))
-            return;
+		if (ob_get_level() && ob_get_length() > 0)
+			ob_clean();
+		$this->getList($this->context->language->id);
+		if (!count($this->_list))
+			return;
 
-        header('Content-type: text/csv');
-        header('Content-Type: application/force-download; charset=ISO-8859-2');
-        header('Cache-Control: no-store, no-cache');
-        header('Content-disposition: attachment; filename="'.$this->className.'_'.date('Y-m-d_His').'.csv"');
+		header('Content-type: text/csv');
+		header('Content-Type: application/force-download; charset=ISO-8859-2');
+		header('Cache-Control: no-store, no-cache');
+		header('Content-disposition: attachment; filename="'.$this->className.'_'.date('Y-m-d_His').'.csv"');
 
-        $megrendelok = [];
-        foreach ($collection as $i => $orderInvoice) {
-            $order = new Order((int)$orderInvoice->id_order);
-            $customer = new Customer((int)$order->id_customer);
-            $cart = new CartCore((int) $order->id_cart);
+		$megrendelok = [];
+		foreach ($collection as $i => $orderInvoice) {
+			$order = new Order((int)$orderInvoice->id_order);
+			$customer = new Customer((int)$order->id_customer);
+			$cart = new CartCore((int) $order->id_cart);
 
-            $megrendelok[$i] = [
-                'order' => $order,
-                'order_invoice' => $orderInvoice,
-                'customer' => $customer,
-                'weight' => $cart->getTotalWeight(),
-                'address' => new Address((int)$order->id_address_invoice),
-            ];
-        }
-        //var_dump($megrendelok);
-        
-        $this->context->smarty->assign(
-            [
-             'megrendelok' => $megrendelok
-            ]
-        );
+			$megrendelok[$i] = [
+				'order' => $order,
+				'order_invoice' => $orderInvoice,
+				'customer' => $customer,
+				'weight' => $cart->getTotalWeight(),
+				'address' => new Address((int)$order->id_address_invoice),
+			];
+		}
+		//var_dump($megrendelok);
+		
+		$this->context->smarty->assign(
+			[
+			 'megrendelok' => $megrendelok
+			]
+		);
 
-        $this->layout = dirname(__FILE__) . '/../../views/templates/admin/csv.tpl';
+		$this->layout = dirname(__FILE__) . '/../../views/templates/admin/csv.tpl';
     }
 }
